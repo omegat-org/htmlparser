3320235c4569edae5646e2a245e7fa8f20ae4c06
Somik Raha <somik@users.sourceforge.net>
2002-06-07 04:42:28 +0000

    uptodate

--- src/com/kizna/html/HTMLParser.java
+++ src/com/kizna/html/HTMLParser.java
@@ -117,7 +117,7 @@ public class HTMLParser
 	 */
 	protected boolean readFlag = false;
 	private Vector scanners = new Vector();
-	public final static java.lang.String VERSION_STRING = "1.2 (Nightly Build May 07, 2002)";
+	public final static java.lang.String VERSION_STRING = "1.2 (Integration Build May 12, 2002)";
 /**
  * This constructor enables the construction of test cases, with readers
  * associated with test string buffers. 
