a6a5887ca3843417439aa9db31dd321773100887
Somik Raha <somik@users.sourceforge.net>
2002-10-31 04:17:25 +0000

    Added/Modified headers

--- src/com/kizna/html/HTMLNode.java
+++ src/com/kizna/html/HTMLNode.java
@@ -16,7 +16,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 // For any questions or suggestions, you can write to me at :
-// Email :somik@kizna.com
+// Email :somik@users.sourceforge.net
 // 
 // Postal Address : 
 // Somik Raha
