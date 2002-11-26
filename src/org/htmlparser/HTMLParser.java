cb4a58227783a36baa081ab2f886744728e16c13
Somik Raha <somik@users.sourceforge.net>
2002-11-26 06:23:26 +0000

    Updated headers

--- src/org/htmlparser/HTMLParser.java
+++ src/org/htmlparser/HTMLParser.java
@@ -1,4 +1,4 @@
-// HTMLParser Library v1_2_20021120 - A java-based parser for HTML
+// HTMLParser Library v1_2_20021125 - A java-based parser for HTML
 // Copyright (C) Dec 31, 2000 Somik Raha
 //
 // This library is free software; you can redistribute it and/or
@@ -131,7 +131,7 @@ public class HTMLParser
 	// Please dont change the formatting of the VERSION_STRING
 	// below. This is done so as to facilitate the ant script
 	public final static java.lang.String 
-	VERSION_STRING="1.2 (Integration Build Nov 20, 2002)"
+	VERSION_STRING="1.2 (Integration Build Nov 25, 2002)"
 	;
 	// End of formatting
 	/**
