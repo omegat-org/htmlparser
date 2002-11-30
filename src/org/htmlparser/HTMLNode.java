c42b3c25f31216ead588647e3db8d61323e7e2fe
Somik Raha <somik@users.sourceforge.net>
2002-11-30 19:15:08 +0000

    Formatting improvements

--- src/org/htmlparser/HTMLNode.java
+++ src/org/htmlparser/HTMLNode.java
@@ -65,10 +65,12 @@ public abstract class HTMLNode
 	 * Return the beginning position of the element
 	 */
 	public abstract int elementBegin();
+	
 	/**
 	 * Return the ending position of the element
 	 */
 	public abstract int elementEnd();
+	
 	/**
 	 * Print the contents of the html node. This method cannot be overridden. It makes a call to the node's
 	 * toString() method.
@@ -76,6 +78,7 @@ public abstract class HTMLNode
 	public final void print() {
 		System.out.println(toString());
 	}
+	
 	/**
 	 * Prints the contents of the node (by a call to toString()) to the print writer provided.
 	 * This method cannot be overridden.
@@ -83,6 +86,7 @@ public abstract class HTMLNode
 	public final void print(PrintWriter pw) {
 		pw.println(toString());
 	} 
+	
 	/**
 	 * Returns a string representation of the node. This is an important method, it allows a simple string transformation
 	 * of a web page, regardless of a node.<br>
@@ -96,12 +100,14 @@ public abstract class HTMLNode
 	 * </pre>
 	 */
 	public abstract String toPlainTextString();
+	
 	/**
 	 * This method will make it easier when using html parser to reproduce html pages (with or without modifications)
 	 * Applications reproducing html can use this method on nodes which are to be used or transferred as they were 
 	 * recieved, with the original html
 	 */
 	public abstract String toHTML();
+	
 	/**
 	 * Return the string representation of the node.
 	 * Subclasses must define this method, and this is typically to be used in the manner<br>
@@ -109,6 +115,7 @@ public abstract class HTMLNode
 	 * @return java.lang.String
 	 */
 	public abstract String toString();
+	
 	/**
 	 * Return the string representation of the node as per the renderer.
 	 * This allows plugging in of user-defined renderers, which can render
