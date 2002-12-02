65ae21c2b9627c3484a1ffec55b21535d16f4dcc
Somik Raha <somik@users.sourceforge.net>
2002-12-02 00:29:53 +0000

    Refactored, formatted

--- src/org/htmlparser/tests/utilTests/HTMLParameterParserTest.java
+++ src/org/htmlparser/tests/utilTests/HTMLParameterParserTest.java
@@ -37,99 +37,87 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class HTMLParameterParserTest extends TestCase {
-
-	/**
-	 * Constructor for HTMLParameterParserTest.
-	 * @param arg0
-	 */
-	public HTMLParameterParserTest(String arg0) {
-		super(arg0);
+	private HTMLParameterParser parser;
+	private HTMLTag tag;
+	private Hashtable table;
+	
+	public HTMLParameterParserTest(String name) {
+		super(name);
 	}
+
+	protected void setUp() {
+		parser = new HTMLParameterParser();
+	}
+	
+	public void getParameterTableFor(String tagContents) {
+		tag = new HTMLTag(0,0,tagContents,"");
+		table = parser.parseParameters(tag);
+		
+	}
+	
 	public void testParseParameters() {
-		HTMLParameterParser parser = new HTMLParameterParser();
-		HTMLTag tag = new HTMLTag(0,0,"a b = \"c\"","");
-		Hashtable table = parser.parseParameters(tag);
+		getParameterTableFor("a b = \"c\"");
 		assertEquals("Value","c",table.get("B"));
 	}
+
 	public void testParseTokenValues() {
-		HTMLParameterParser parser = new HTMLParameterParser();
-		HTMLTag tag = new HTMLTag(0,0,"a b = \"'\"","");
-		Hashtable table = parser.parseParameters(tag);
+		getParameterTableFor("a b = \"'\"");
 		assertEquals("Value","'",table.get("B"));
 	}
         
 	public void testParseEmptyValues() {
-		HTMLParameterParser parser = new HTMLParameterParser();
-		HTMLTag tag = new HTMLTag(0,0,"a b = \"\"","");
-		Hashtable table = parser.parseParameters(tag);
+		getParameterTableFor("a b = \"\"");
 		assertEquals("Value","",table.get("B"));		
 	}
 
 	public void testParseMissingEqual() {
-		HTMLParameterParser parser = new HTMLParameterParser();
-		HTMLTag tag = new HTMLTag(0,0,"a b\"c\"","");
-		Hashtable table = parser.parseParameters(tag);
+		getParameterTableFor("a b\"c\"");
 		assertEquals("ValueB","",table.get("B"));
                 
 	}
         
-        public void testTwoParams(){
-		HTMLParameterParser parser = new HTMLParameterParser();
-		HTMLTag tag = new HTMLTag(0,0,"PARAM NAME=\"Param1\" VALUE=\"Somik\">\n","");
-		Hashtable table = parser.parseParameters(tag);
+    public void testTwoParams(){
+		getParameterTableFor("PARAM NAME=\"Param1\" VALUE=\"Somik\">\n");
 		assertEquals("Param1","Param1",table.get("NAME"));
-                assertEquals("Somik","Somik",table.get("VALUE"));                
-        }
+		assertEquals("Somik","Somik",table.get("VALUE"));                
+    }
 
-        public void testPlainParams(){
-            HTMLParameterParser parser = new HTMLParameterParser();
-            HTMLTag tag = new HTMLTag(0,0,"PARAM NAME=Param1 VALUE=Somik","");
-            Hashtable table = parser.parseParameters(tag);
-            assertEquals("Param1","Param1",table.get("NAME"));
-            assertEquals("Somik","Somik",table.get("VALUE"));                
-        }
-        
-        public void testValueMissing() {
-            HTMLParameterParser parser = new HTMLParameterParser();
-            HTMLTag tag = new HTMLTag(0,0,"INPUT type=\"checkbox\" name=\"Authorize\" value=\"Y\" checked","");
-            Hashtable table = parser.parseParameters(tag);
-            assertEquals("Name of Tag","INPUT",table.get(HTMLTag.TAGNAME));
-            assertEquals("Type","checkbox",table.get("TYPE"));                
-            assertEquals("Name","Authorize",table.get("NAME"));
-            assertEquals("Value","Y",table.get("VALUE"));
-            assertEquals("Checked","",table.get("CHECKED"));
-        }
-        /**
-         * This is a simulation of a bug reported by Dhaval Udani - wherein 
-         * a space before the end of the tag causes a problem - there is a key
-         * in the table with just a space in it and an empty value
-         */
-        public void testIncorrectSpaceKeyBug() {
-            HTMLParameterParser parser = new HTMLParameterParser();
-            HTMLTag tag = new HTMLTag(0,0,"TEXTAREA name=\"Remarks\" ","");
-            Hashtable table = parser.parseParameters(tag);
-			// There should only be two keys..
-			assertEquals("There should only be two keys",2,table.size());
-			// The first key is name
-			String key1 = "NAME";
-			String value1 = (String)table.get(key1);
-			assertEquals("Expected value 1", "Remarks",value1);
-			String key2 = HTMLTag.TAGNAME;
-			assertEquals("Expected Value 2","TEXTAREA",table.get(key2));
-        }
-        public void testNullTag(){
-            HTMLParameterParser parser = new HTMLParameterParser();
-            HTMLTag tag = new HTMLTag(0,0,"INPUT type=","");
-            Hashtable table = parser.parseParameters(tag);
-            assertEquals("Name of Tag","INPUT",table.get(HTMLTag.TAGNAME));
-            assertEquals("Type","",table.get("TYPE"));                
+    public void testPlainParams(){
+        getParameterTableFor("PARAM NAME=Param1 VALUE=Somik");
+        assertEquals("Param1","Param1",table.get("NAME"));
+        assertEquals("Somik","Somik",table.get("VALUE"));                
+    }
+    
+    public void testValueMissing() {
+        getParameterTableFor("INPUT type=\"checkbox\" name=\"Authorize\" value=\"Y\" checked");
+        assertEquals("Name of Tag","INPUT",table.get(HTMLTag.TAGNAME));
+        assertEquals("Type","checkbox",table.get("TYPE"));                
+        assertEquals("Name","Authorize",table.get("NAME"));
+        assertEquals("Value","Y",table.get("VALUE"));
+        assertEquals("Checked","",table.get("CHECKED"));
+    }
 
-        }
-        
-	public static TestSuite suite() {
-            //TestSuite t = new TestSuite();
-            //t.addTest(new HTMLParameterParserTest("testValueMissing"));
-            //return t;
-            return new TestSuite(HTMLParameterParserTest.class);
-	}
+    /**
+     * This is a simulation of a bug reported by Dhaval Udani - wherein 
+     * a space before the end of the tag causes a problem - there is a key
+     * in the table with just a space in it and an empty value
+     */
+    public void testIncorrectSpaceKeyBug() {
+        getParameterTableFor("TEXTAREA name=\"Remarks\" ");
+		// There should only be two keys..
+		assertEquals("There should only be two keys",2,table.size());
+		// The first key is name
+		String key1 = "NAME";
+		String value1 = (String)table.get(key1);
+		assertEquals("Expected value 1", "Remarks",value1);
+		String key2 = HTMLTag.TAGNAME;
+		assertEquals("Expected Value 2","TEXTAREA",table.get(key2));
+    }
+
+    public void testNullTag(){
+        getParameterTableFor("INPUT type=");
+        assertEquals("Name of Tag","INPUT",table.get(HTMLTag.TAGNAME));
+        assertEquals("Type","",table.get("TYPE"));                
+
+    }
 }
--- src/org/htmlparser/tests/utilTests/HTMLTagParserTest.java
+++ src/org/htmlparser/tests/utilTests/HTMLTagParserTest.java
@@ -37,42 +37,31 @@ import org.htmlparser.tests.tagTests.HTMLTagTest;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * @author Somik Raha
- *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
- */
 public class HTMLTagParserTest extends HTMLParserTestCase {
 	private HTMLTagParser tagParser;
-	/**
-	 * Constructor for HTMLTagParserTest.
-	 * @param arg0
-	 */
-	public HTMLTagParserTest(String arg0) {
-		super(arg0);
+
+	public HTMLTagParserTest(String name) {
+		super(name);
 	}
+
     public void testCorrectTag() {
     	HTMLTag tag = new HTMLTag(0,20,"font face=\"Arial,\"helvetica,\" sans-serif=\"sans-serif\" size=\"2\" color=\"#FFFFFF\"","<font face=\"Arial,\"helvetica,\" sans-serif=\"sans-serif\" size=\"2\" color=\"#FFFFFF\">");
 		tagParser.correctTag(tag);
 		assertStringEquals("Corrected Tag","font face=\"Arial,helvetica,\" sans-serif=\"sans-serif\" size=\"2\" color=\"#FFFFFF\"",tag.getText());
     }	
+
 	public void testInsertInvertedCommasCorrectly() {
 		StringBuffer test = new StringBuffer("a b=c d e = f"); 
 		StringBuffer result = tagParser.insertInvertedCommasCorrectly(test);
 		assertStringEquals("Expected Correction","a b=\"c d\" e=\"f\"",result.toString());
-		
 	}
+
 	public void testPruneSpaces() {
 		String test = "  fdfdf dfdf   ";
 		assertEquals("Expected Pruned string","fdfdf dfdf",tagParser.pruneSpaces(test));
 	}   
+
 	protected void setUp() {
 		tagParser = new HTMLTagParser(new DefaultHTMLParserFeedback());	
 	} 
-	public static TestSuite suite() {
-		return new TestSuite(HTMLTagParserTest.class);
-	}
 }
