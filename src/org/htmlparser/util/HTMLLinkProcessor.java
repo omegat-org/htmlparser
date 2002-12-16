e23d7dad13a533372b1593750ecacbdf9e2129eb
Derrick Oswald <derrickoswald@users.sourceforge.net>
2002-12-16 05:00:58 +0000

    rewrite extract() so URL class does all the heavy lifting

--- src/org/htmlparser/util/HTMLLinkProcessor.java
+++ src/org/htmlparser/util/HTMLLinkProcessor.java
@@ -27,165 +27,169 @@
 // Website : http://www.industriallogic.com
 
 package org.htmlparser.util;
+
+import java.net.URL;
+import java.net.MalformedURLException;
 import java.util.Enumeration;
 import java.util.Vector;
+
 /**
  * Processor class for links, is present basically as a utility class.
  */
-public class HTMLLinkProcessor {
-	private String baseUrl;
-	/**
-	 * HTMLExtractor constructor comment.
-	 */
-	public HTMLLinkProcessor() {
-		this.baseUrl=null;
-	}
-	/**
-	 * Parse a string and add all the directories found into a vector
-	 * @directories Directory vector
-	 * @temp String to be parsed
-	 */ 
-	public void addDirectoriesToVector(Vector directories, String temp) {
-		int n;			
-		do
-		{
-			n = temp.indexOf("/");
-			if (n!=-1)
-			{
-				directories.addElement(temp.substring(0,n+1));
-				temp=temp.substring(n+1,temp.length());
-			}
-		}
-		while (n!=-1);
-		// Check if remaining value of temp, has any .htm in it. If not, add it to the directories
-		if (temp.length()>0)
-		{
-			int dotPos =  temp.toUpperCase().indexOf(".");
-			int wwwPos = temp.toUpperCase().indexOf("WWW");
-			if (dotPos==-1 || wwwPos!=-1)
-			directories.addElement(temp+'/');
-		}
-	}
-	/**
-	 * Check if a given link is relative, if yes, add the 
-	 * base url to it and return, else return unmodified.
-	 * @param link The link to be checked
-	 * @param url The base url
-	 * @return String Absolute URL
-	 */
-	public String checkIfLinkIsRelative(String link, String url) {
-		if (link==null || link.length()==0) return "";
-		int slashLoc = link.indexOf("://");
-		int queryLoc = link.indexOf('?');
-		if ((slashLoc==-1 || (queryLoc!=-1 && queryLoc<slashLoc)) && (link.indexOf("javascript:") == -1) && link.indexOf("mailto:")==-1 && url != null)
-		{
-			// Bug fix by Karem for relative links that
-			// begin with /
-			if (link.charAt(0)=='/') { 
-				link = processSlashIsFirstChar(link, url);
-			} else {
-				if (baseUrl!=null) link = baseUrl+"/"+link; else
-				link = processAppendRelativeLink(link, url);	
-			}
-		}
-		return link;
-	}
-	public boolean doesQueryComeBeforeSlash(int slashLoc, int queryLoc) {
-		return (queryLoc!=-1 && queryLoc<slashLoc);
-	}
-	public String processAppendRelativeLink(String link, String url) {
-		String temp=url;
-		Vector directories = new Vector();
-		addDirectoriesToVector(directories, temp);
-		link = handleRelationalPath(directories, link);
-		String dir = createDirectory(directories);
-		link = removeFirstSlashIfFound(link);
-		link=dir+link;	
-		return link;
-	}
-	public String processSlashIsFirstChar(String link, String url) {
-		int i = url.indexOf("/",7); 
-		if (i==-1) { 
-			link = url+link; 
-		} else { 
-			link = url.substring(0,i)+link; 
-		} 
-		return link;
-	}
-	/** 
-	 * Create the directory by summing all the contents
-	 * of the directory vector
-	 * @param directories
-	 * @return String The Directory string
-	 */
-	public String createDirectory(Vector directories) {
-		String dir="";			
-		for (Enumeration e = directories.elements();e.hasMoreElements();)
-		{
-			dir += (String)e.nextElement();
-		}
-		return dir;
-	}
-	
-	public String extract(String link,String url) throws HTMLParserException {
-		try {
-			link = checkIfLinkIsRelative(link, url);
-			
-			// remove numeric character references and character entity references
-            link = Translate.decode (link);
-			return link;
-		}
-		catch (Exception e) {
-			throw new HTMLParserException("HTMLLinkProcessor.extract() : Error in extraction, link = "+link+", url = "+url,e);
-		}
-	}
-	public String handleRelationalPath(Vector directories, String link) {
-		int dotLoc = link.indexOf("..");
-		while (dotLoc==0)
-		{
-			link = link.substring(3,link.length());
-			if (directories.size()>1)
-			directories.removeElementAt(directories.size()-1);
-			dotLoc=link.indexOf("..");
-		}
-		return link;
-	}
-	public static String fixSpaces(String url) {
-		int index = url.indexOf(' ');
-		if (index==-1) return url; else {
-			StringBuffer returnURL=new StringBuffer(url.substring(0,index));
-			char ch;
-			for (int i=index;i<url.length();i++) {
-				ch = url.charAt(i);
-				if (ch==' ') returnURL.append("%20"); else
-				returnURL.append(ch);
-			}
-			return returnURL.toString();
-		}
-	}
-	public String removeFirstSlashIfFound(String link) {
-		if (link==null || link.length()==0) return null;
-		if (link.charAt(0)=='/' || link.charAt(0)=='\\')
-		link=link.substring(1,link.length());
-		return link;
-	}
-	public static boolean isURL(String resourceLocn) {
-		return resourceLocn.indexOf("http")==0 || resourceLocn.indexOf("www.")==0 || resourceLocn.indexOf("file://")==0;
-	}	
-	/**
-	 * Returns the baseUrl.
-	 * @return String
-	 */
-	public String getBaseUrl() {
-		return baseUrl;
-	}
+public class HTMLLinkProcessor
+{
+    /**
+     * Overriding base URL.
+     * If set, this is used instead of a provided base URL in extract().
+     */
+    private String baseUrl;
 
-	/**
-	 * Sets the baseUrl.
-	 * @param baseUrl The baseUrl to set
-	 */
-	public void setBaseUrl(String baseUrl) {
-		this.baseUrl = baseUrl;
-	}
+    /**
+     * Create an HTMLLinkProcessor.
+     */
+    public HTMLLinkProcessor ()
+    {
+        baseUrl = null;
+    }
 
+    /**
+     * Create an absolute URL from a possibly relative link and a base URL.
+     * @param link The reslative portion of a URL.
+     * @param base The base URL unless overridden by the current baseURL property.
+     * @return The fully qualified URL or the original link if a failure occured.
+     */
+    public String extract (String link, String base)
+        throws
+            HTMLParserException
+    {
+        URL url; // constructed URL combining relative link and base
+        String path; // path portion of constructed URL
+        boolean modified; // true if path is modified by us
+        boolean absolute; // true if link starts with "/"
+        int index;
+        String ret;
+        
+        try
+        {
+            if (null == link)
+                link = "";
+            if (null != getBaseUrl ())
+                base = getBaseUrl ();
+            if ((null == base) || ("".equals (link)))
+                ret = link;
+            else
+            {
+                url = new URL (new URL (base), link);
+                path = url.getPath ();
+                modified = false;
+                absolute = link.startsWith ("/");
+                if (!absolute)
+                {   // we prefer to fix incorrect relative links
+                    // this doesn't fix them all, just the ones at the start
+                    while (path.startsWith ("/."))
+                    {
+                        if (path.startsWith ("/../"))
+                        {
+                            path = path.substring (3);
+                            modified = true;
+                        }
+                        else if (path.startsWith ("/./"))
+                        {
+                            path = path.substring (2);
+                            modified = true;
+                        }
+                    }
+                }
+                // fix backslashes
+                while (-1 != (index = path.indexOf ("/\\")))
+                {
+                    path = path.substring (0, index + 1) + path.substring (index + 2);
+                    modified = true;
+                }
+                if (modified)
+                    url = new URL (url, path);
+                ret = url.toExternalForm ();
+            }
+        }
+        catch (MalformedURLException murle)
+        {
+            ret = link;
+        }
+        
+        return (Translate.decode (ret));
+    }
+
+    /**
+     * Turn spaces into %20.
+     * @param url The url containing spaces.
+     * @return The URL with spaces as %20 sequences.
+     */
+    public static String fixSpaces (String url)
+    {
+        int index;
+        int length;
+        char ch;
+        StringBuffer returnURL;
+        
+        index = url.indexOf (' ');
+        if (-1 != index)
+        {
+            length = url.length ();
+            returnURL = new StringBuffer (length * 3);
+            returnURL.append (url.substring (0, index));
+            for (int i = index; i < length; i++)
+            {
+                ch = url.charAt (i);
+                if (ch==' ')
+                    returnURL.append ("%20");
+                else
+                    returnURL.append (ch);
+            }
+            url = returnURL.toString ();
+        }
+        
+        return (url);
+    }
+    
+    /**
+     * Check if a resource is a valid URL.
+     * @param resourceLocn The resource to test.
+     * @return <code>true</code> if the resource is a valid URL.
+     */
+    public static boolean isURL (String resourceLocn)
+    {
+        URL url;
+        boolean ret;
+        
+        try
+        {
+            url = new URL (resourceLocn);
+            ret = true;
+        }
+        catch (MalformedURLException murle)
+        {
+            ret = false;
+        }
+        
+        return (ret);
+    }
+    
+    /**
+     * Returns the baseUrl.
+     * @return String
+     */
+    public String getBaseUrl ()
+    {
+        return baseUrl;
+    }
+    
+    /**
+     * Sets the baseUrl.
+     * @param baseUrl The baseUrl to set
+     */
+    public void setBaseUrl (String baseUrl)
+    {
+        this.baseUrl = baseUrl;
+    }
+    
 }
