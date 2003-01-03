01da2f2900c048fee664584a05250f2baa9a0dfb
Somik Raha <somik@users.sourceforge.net>
2003-01-03 22:28:26 +0000

    opened the interface

--- src/org/htmlparser/HTMLVisitor.java
+++ src/org/htmlparser/HTMLVisitor.java
@@ -3,6 +3,8 @@ package org.htmlparser;
 import org.htmlparser.tags.HTMLTag;
 
 public abstract class HTMLVisitor {
-	public abstract void visitTag(HTMLTag tag);
-	public abstract void visitStringNode(HTMLStringNode stringNode);
+	public void visitTag(HTMLTag tag) {
+	}
+	public void visitStringNode(HTMLStringNode stringNode) {
+	}
 }
