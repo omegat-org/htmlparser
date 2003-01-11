7459dc28ec296139a22854aeadadd3a3a4e37090
Somik Raha <somik@users.sourceforge.net>
2003-01-11 07:21:55 +0000

    Refactored HTMLFrameSetScanner to use HTMLCompositeTagScanner

--- src/org/htmlparser/scanners/HTMLFrameSetScanner.java
+++ src/org/htmlparser/scanners/HTMLFrameSetScanner.java
@@ -33,18 +33,10 @@ package org.htmlparser.scanners;
 //////////////////
 // Java Imports //
 //////////////////
-import java.util.Enumeration;
-import java.util.Vector;
-
-import org.htmlparser.HTMLNode;
-import org.htmlparser.HTMLReader;
-import org.htmlparser.tags.HTMLEndTag;
 import org.htmlparser.tags.HTMLFrameSetTag;
-import org.htmlparser.tags.HTMLFrameTag;
 import org.htmlparser.tags.HTMLTag;
 import org.htmlparser.tags.data.HTMLCompositeTagData;
 import org.htmlparser.tags.data.HTMLTagData;
-import org.htmlparser.util.HTMLParserException;
 
 /**
  * Scans for the Frame Tag. This is a subclass of HTMLTagScanner, and is called using a
@@ -52,104 +44,28 @@ import org.htmlparser.util.HTMLParserException;
  * given string contains an image tag. Extraction is done by the scan method thereafter
  * by the user of this class.
  */
-public class HTMLFrameSetScanner extends HTMLTagScanner
+public class HTMLFrameSetScanner extends HTMLCompositeTagScanner
 {
-	/**
-	 * Overriding the default constructor
-	 */
 	public HTMLFrameSetScanner()
 	{
-		super();
-	}
-	/**
-	 * Overriding the constructor to accept the filter
-	 */
-	public HTMLFrameSetScanner(String filter)
-	{
-		super(filter);
-	}
-	/**
-	 * Template Method, used to decide if this scanner can handle the Image tag type. If
-	 * the evaluation returns true, the calling side makes a call to scan().
-	 * @param s The complete text contents of the HTMLTag.
-	 */
-	public boolean evaluate(String s, HTMLTagScanner previousOpenScanner)
-	{
-		// Eat up leading blanks
-		s = absorbLeadingBlanks(s);
-		if (s.toUpperCase().indexOf("FRAMESET")==0)
-		return true; else return false;			
+		super("FRAMESET");
 	}
 
-	/**
-	 * Scan the tag and extract the information related to the <IMG> tag. The url of the
-	 * initiating scan has to be provided in case relative links are found. The initial
-	 * url is then prepended to it to give an absolute link.
-	 * The HTMLReader is provided in order to do a lookahead operation. We assume that
-	 * the identification has already been performed using the evaluate() method.
-	 * @param tag HTML Tag to be scanned for identification
-	 * @param url The initiating url of the scan (Where the html page lies)
-	 * @param reader The reader object responsible for reading the html page
-	 * @param currentLine The current line (automatically provided by HTMLTag)
-	 */
-	public HTMLTag scan(HTMLTag tag,String url,HTMLReader reader,String currentLine) throws HTMLParserException
+	public HTMLFrameSetScanner(String filter)
 	{
-		try {
-			HTMLTag startTag = tag;
-			HTMLTag endTag   = null;
-			Vector frameVector = new Vector();
-			boolean endFrameSetFound=false;
-			HTMLNode node;
-			int frameSetEnd=-1;
-			
-			do {
-				node = reader.readElement();
-				if (node instanceof HTMLEndTag) {
-					endTag = (HTMLEndTag)node;
-					if (endTag.getText().toUpperCase().equals("FRAMESET")) {
-						endFrameSetFound = true;
-						frameSetEnd = endTag.elementEnd();
-					}
-				} else
-				if (node instanceof HTMLFrameTag) {
-					frameVector.addElement(node);
-				}
-			}
-			while(!endFrameSetFound && node!=null);
-			if (node==null && endFrameSetFound== false) {
-				StringBuffer msg = new StringBuffer();
-				for (Enumeration e = frameVector.elements();e.hasMoreElements();) {
-					msg.append((HTMLNode)e.nextElement()+"\n");
-				}
-				throw new HTMLParserException("HTMLFrameSetScanner.scan() : Went into a potential infinite loop - tags must be malformed.\n"+
-				"Frame Vector contents : "+msg.toString());
-			}
-			HTMLFrameSetTag frameSetTag = new HTMLFrameSetTag(
-				new HTMLTagData(
-					tag.elementBegin(),
-					frameSetEnd,
-					tag.getText(),
-					currentLine
-				),
-				new HTMLCompositeTagData(
-					startTag,
-					endTag,
-					frameVector
-				)
-			);
-			return frameSetTag;		
-		}
-		catch (Exception e) {
-			throw new HTMLParserException("HTMLFrameSetScanner.scan() : Error while scanning Frameset tag, current line = "+currentLine,e);
-		}
+		super(filter,"FRAMESET");
 	}
-	/**
-	 * @see org.htmlparser.scanners.HTMLTagScanner#getID()
-	 */
+
 	public String [] getID() {
 		String [] ids = new String[1];
 		ids[0] = "FRAMESET";
 		return ids;
+	}
+
+	protected HTMLTag createTag(
+		HTMLTagData tagData,
+		HTMLCompositeTagData compositeTagData) {
+		return new HTMLFrameSetTag(tagData,compositeTagData);
 	}
 
 }
