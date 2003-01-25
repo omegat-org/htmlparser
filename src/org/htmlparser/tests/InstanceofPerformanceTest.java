a6060cae9a0c7c7444520e4a10abe34c35551cc2
Somik Raha <somik@users.sourceforge.net>
2003-01-25 23:49:16 +0000

    updated

--- src/org/htmlparser/tests/InstanceofPerformanceTest.java
+++ src/org/htmlparser/tests/InstanceofPerformanceTest.java
@@ -1,31 +1,41 @@
 package org.htmlparser.tests;
 
+import java.util.Enumeration;
+import java.util.Vector;
+
 import org.htmlparser.HTMLNode;
 import org.htmlparser.HTMLParser;
-import org.htmlparser.HTMLStringNode;
-import org.htmlparser.scanners.HTMLLinkScanner;
-import org.htmlparser.tags.HTMLEndTag;
-import org.htmlparser.tags.HTMLTag;
+import org.htmlparser.tags.HTMLFormTag;
+import org.htmlparser.tests.scannersTests.HTMLFormScannerTest;
 import org.htmlparser.util.HTMLEnumeration;
+import org.htmlparser.util.SimpleEnumeration;
 
 public class InstanceofPerformanceTest {
-	HTMLNode node;
+	HTMLFormTag formTag;
+	Vector formChildren;
 	public void setUp() throws Exception {
 		HTMLParser parser =
 			HTMLParser.createParser(
-				"<A HREF=\"somelink.html\">sometext</A>"
+				HTMLFormScannerTest.FORM_HTML
 			);
-		parser.addScanner(new HTMLLinkScanner(""));
+		parser.registerScanners();
 		HTMLEnumeration e = parser.elements();
-		node = e.nextNode();
+		HTMLNode node = e.nextNode();
+		formTag = (HTMLFormTag)node;
+		formChildren = new Vector();
+		for (SimpleEnumeration se = formTag.children();se.hasMoreNodes();) {
+			formChildren.addElement(se.nextNode());
+		}
 	}
+	
 	public void doInstanceofTest(long [] time,int index, long numTimes) {
 		System.out.println("doInstanceofTest("+index+")");
 		long start = System.currentTimeMillis();
-		for (long i=0;i<numTimes;i++)
-			if (node instanceof HTMLTag) {}
-			else if (node instanceof HTMLEndTag) {}
-			else if (node instanceof HTMLStringNode) {}
+		for (long i=0;i<numTimes;i++) {
+			for (Enumeration e = formChildren.elements();e.hasMoreElements();) {
+				HTMLNode node = (HTMLNode)e.nextElement();				
+			}
+		}
 		long end = System.currentTimeMillis();
 		time[index] = end-start;
 	}
@@ -33,24 +43,25 @@ public class InstanceofPerformanceTest {
 	public void doGetTypeTest(long [] time,int index, long numTimes) {
 		System.out.println("doGetTypeTest("+index+")");
 		long start = System.currentTimeMillis();
-		for (long i=0;i<numTimes;i++)
-			if (node.getType()==HTMLTag.TYPE) {} 
-			else if (node.getType()==HTMLEndTag.TYPE) {}
-			else if (node.getType()==HTMLStringNode.TYPE) {}
+		for (long i=0;i<numTimes;i++) {
+			for (SimpleEnumeration e = formTag.children();e.hasMoreNodes();) {
+				HTMLNode node = e.nextNode();
+			}
+		}
 		long end = System.currentTimeMillis();
 		time[index] = end-start;
 	}
 
 	public void perform() {
-		int numTimes = 50;
+		int numTimes = 30;
 		long time1[] = new long[numTimes], 
 		time2[] = new long[numTimes];
 		
 		for (int i=0;i<numTimes;i++)
-			doInstanceofTest(time1,i,i*10000000);
+			doInstanceofTest(time1,i,i*10000);
 		
 		for (int i=0;i<numTimes;i++)
-			doGetTypeTest(time2,i,i*10000000);
+			doGetTypeTest(time2,i,i*10000);
 		
 		print(time1,time2);
 	}
