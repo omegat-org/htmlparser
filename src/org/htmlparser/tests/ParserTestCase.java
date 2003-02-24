39c92358343767fb65846ec50e95515b63d927e9
Somik Raha <somik@users.sourceforge.net>
2003-02-24 02:14:37 +0000

    improved assertStringEquals

--- src/org/htmlparser/tests/ParserTestCase.java
+++ src/org/htmlparser/tests/ParserTestCase.java
@@ -89,27 +89,40 @@ public class ParserTestCase extends TestCase {
 			if (
 					(expected.length() != actual.length() && 
 						(
-							i == (expected.length()-1 ) || 
-							i == (actual.length()-1 )
+							i >= (expected.length()-1 ) || 
+							i >= (actual.length()-1 )
 						)
 					) || 
 					(actual.charAt(i) != expected.charAt(i))
 				) {
-				assertTrue(message +mismatchInfo + " \nMismatch of strings at char posn " + i + 
-						   " \n\nString Expected upto mismatch = " + 
-						   expected.substring(0, i) + 
-						   " \n\nString Actual upto mismatch = " + 
-						   actual.substring(0, i) + 
-						   " \n\nString Expected MISMATCH CHARACTER = " + 
-						   expected.charAt(i) + ", code = " + 
-						   (int) expected.charAt(i) + 
-						   " \n\nString Actual MISMATCH CHARACTER = " + 
-						   actual.charAt(i) + ", code = " + 
-						   (int) actual.charAt(i) + 
-						   " \n\n**** COMPLETE STRING EXPECTED ****\n" + 
-						   expected + 
-						   " \n\n**** COMPLETE STRING ACTUAL***\n" + actual, 
-						   false);
+					StringBuffer errorMsg = new StringBuffer();
+					errorMsg.append(
+					 	message +mismatchInfo + " \nMismatch of strings at char posn " + i + 
+					   	" \n\nString Expected upto mismatch = " + 
+					   	expected.substring(0, i) + 
+					   	" \n\nString Actual upto mismatch = " + 
+					   	actual.substring(0, i)
+					);
+					if (i<expected.length())			   	
+					   errorMsg.append(
+							" \n\nString Expected MISMATCH CHARACTER = "+ 
+					   		expected.charAt(i) + ", code = " +
+							(int) expected.charAt(i)
+						); 
+
+					if (i<actual.length())
+						errorMsg.append(
+					   		" \n\nString Actual MISMATCH CHARACTER = " + 
+					   		actual.charAt(i) + ", code = " + 
+					   		(int) actual.charAt(i)
+					   	); 
+					   
+					errorMsg.append(
+						" \n\n**** COMPLETE STRING EXPECTED ****\n" + 
+					   	expected + 
+					   	" \n\n**** COMPLETE STRING ACTUAL***\n" + actual
+					);
+					assertTrue(errorMsg.toString(),false);
 			}
         	
 		}
