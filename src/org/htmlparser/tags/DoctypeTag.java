ccd8bca227fcb073fc74a12b8de724615360929e
Somik Raha <somik@users.sourceforge.net>
2003-03-24 01:33:34 +0000

    latest release

--- src/org/htmlparser/tags/DoctypeTag.java
+++ src/org/htmlparser/tags/DoctypeTag.java
@@ -1,4 +1,4 @@
-// HTMLParser Library v1_3_20030316 - A java-based parser for HTML
+// HTMLParser Library v1_3_20030323 - A java-based parser for HTML
 // Copyright (C) Dec 31, 2000 Somik Raha
 //
 // This library is free software; you can redistribute it and/or
