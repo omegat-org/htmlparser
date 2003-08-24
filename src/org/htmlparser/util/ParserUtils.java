610c81ba9cba1ee2a458da648288aed354e3f464
Somik Raha <somik@users.sourceforge.net>
2003-08-24 18:53:03 +0000

    removed dead code

--- src/org/htmlparser/util/ParserUtils.java
+++ src/org/htmlparser/util/ParserUtils.java
@@ -34,103 +34,71 @@ import java.util.Map;
 
 import org.htmlparser.Node;
 import org.htmlparser.NodeReader;
-import org.htmlparser.scanners.TagScanner;
 import org.htmlparser.tags.Tag;
 
-public class ParserUtils
-{
-	public static boolean evaluateTag(TagScanner pTagScanner, 
-										String pTagString, String pTagName)
-	{
-		pTagString = TagScanner.absorbLeadingBlanks(pTagString);
-		if (pTagString.toUpperCase().indexOf(pTagName)==0)
-			return true; 
-		else 
-			return false;
-	}
+public class ParserUtils {
 	
-	public static String toHTML(Tag tag)
-	{
-		StringBuffer htmlString = new StringBuffer();
-		
-		Hashtable attrs = tag.getAttributes();
-		String pTagName = tag.getAttribute(Tag.TAGNAME);
-		htmlString.append("<").append(pTagName);
-		for (Enumeration e = attrs.keys();e.hasMoreElements();)
-		{
-			String key = (String)e.nextElement();
-			String value = (String)attrs.get(key);
-			if (!key.equalsIgnoreCase(Tag.TAGNAME) && value.length()>0)
-				htmlString.append(" ").append(key).append("=\"").append(value).append("\"");
-		}
-		htmlString.append(">");
-		
-		return htmlString.toString();
-	}
-	
-	public static String toString(Tag tag)
-	{
+	public static String toString(Tag tag) {
 		String tagName = tag.getAttribute(Tag.TAGNAME);
 		Hashtable attrs = tag.getAttributes();
-		
+
 		StringBuffer lString = new StringBuffer(tagName);
 		lString.append(" TAG\n");
 		lString.append("--------\n");
-		
-		for (Enumeration e = attrs.keys();e.hasMoreElements();)
-		{
-			String key = (String)e.nextElement();
-			String value = (String)attrs.get(key);
-			if (!key.equalsIgnoreCase(Tag.TAGNAME) && value.length()>0)
+
+		for (Enumeration e = attrs.keys(); e.hasMoreElements();) {
+			String key = (String) e.nextElement();
+			String value = (String) attrs.get(key);
+			if (!key.equalsIgnoreCase(Tag.TAGNAME) && value.length() > 0)
 				lString.append(key).append(" : ").append(value).append("\n");
 		}
-		
+
 		return lString.toString();
 	}
-	
-	public static Map adjustScanners(NodeReader reader) 
-	{
-		Map tempScanners= new Hashtable();
-		tempScanners = reader.getParser().getScanners();		
+
+	public static Map adjustScanners(NodeReader reader) {
+		Map tempScanners = new Hashtable();
+		tempScanners = reader.getParser().getScanners();
 		// Remove all existing scanners
 		reader.getParser().flushScanners();
 		return tempScanners;
 	}
-	public static void restoreScanners(NodeReader reader, Map tempScanners)
-	{
+	
+	public static void restoreScanners(NodeReader reader, Map tempScanners) {
 		// Flush the scanners
 		reader.getParser().setScanners(tempScanners);
 	}
 
-	public static String removeChars(String s,char occur)  {
-	    StringBuffer newString = new StringBuffer();
-	    char ch;
-	    for (int i=0;i<s.length();i++) {
-	      ch = s.charAt(i);
-	      if (ch!=occur) newString.append(ch);
-	    }
-	    return newString.toString();
+	public static String removeChars(String s, char occur) {
+		StringBuffer newString = new StringBuffer();
+		char ch;
+		for (int i = 0; i < s.length(); i++) {
+			ch = s.charAt(i);
+			if (ch != occur)
+				newString.append(ch);
+		}
+		return newString.toString();
 	}
 
 	public static String removeEscapeCharacters(String inputString) {
-		inputString = ParserUtils.removeChars(inputString,'\r');
-		inputString = ParserUtils.removeChars(inputString,'\n');
-		inputString = ParserUtils.removeChars(inputString,'\t');
+		inputString = ParserUtils.removeChars(inputString, '\r');
+		inputString = ParserUtils.removeChars(inputString, '\n');
+		inputString = ParserUtils.removeChars(inputString, '\t');
 		return inputString;
 	}
 
 	public static String removeLeadingBlanks(String plainText) {
-		while (plainText.indexOf(' ')==0) 
-			plainText=plainText.substring(1);
+		while (plainText.indexOf(' ') == 0)
+			plainText = plainText.substring(1);
 		return plainText;
 	}
 
 	public static String removeTrailingBlanks(String text) {
-		char ch = ' ' ;
-		while (ch==' '){
-			ch = text.charAt(text.length()-1);
-			if (ch==' ') 
-				text = text.substring(0,text.length()-1);				
+		char ch = ' ';
+		while (ch == ' ') {
+			ch = text.charAt(text.length() - 1);
+			if (ch == ' ')
+				text = text.substring(0, text.length() - 1);
 		}
 		return text;
 	}
@@ -144,9 +112,9 @@ public class ParserUtils
 	 */
 	public static Node[] findTypeInNode(Node node, Class type) {
 		NodeList nodeList = new NodeList();
-		node.collectInto(nodeList,type);
-		Node spans [] = nodeList.toNodeArray();
+		node.collectInto(nodeList, type);
+		Node spans[] = nodeList.toNodeArray();
 		return spans;
-	}	
-	
+	}
+
 }
