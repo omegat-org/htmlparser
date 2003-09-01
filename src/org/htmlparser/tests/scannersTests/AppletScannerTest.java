7e4fa7c2686b3e9bca717b059ea8bdf214555ffc
Derrick Oswald <derrickoswald@users.sourceforge.net>
2003-09-01 22:02:48 +0000

    Further to bug #798554 Applet Tag does not update codebase data, fix test case.
    ********************
    Note:
    AppletTag getAttribute() no longer retrieves the parameter, it returns the Tag.getAttribute() value like it should.
    Use AttributeTag.getParameter() to access the list of parameters of the applet tag.,
    ********************

--- src/org/htmlparser/tests/scannersTests/AppletScannerTest.java
+++ src/org/htmlparser/tests/scannersTests/AppletScannerTest.java
@@ -38,51 +38,51 @@ import org.htmlparser.util.ParserException;
 public class AppletScannerTest extends ParserTestCase
 {
 
-	public AppletScannerTest(String name) {
-		super(name);
-	}
-	
-	public void testEvaluate() 
-	{
-		AppletScanner scanner = new AppletScanner("-a");
-		boolean retVal = scanner.evaluate("   Applet ",null);
-		assertEquals("Evaluation of APPLET tag",new Boolean(true),new Boolean(retVal));
-	}
+    public AppletScannerTest(String name) {
+        super(name);
+    }
+    
+    public void testEvaluate() 
+    {
+        AppletScanner scanner = new AppletScanner("-a");
+        boolean retVal = scanner.evaluate("   Applet ",null);
+        assertEquals("Evaluation of APPLET tag",new Boolean(true),new Boolean(retVal));
+    }
 
-	public void testScan() throws ParserException
-	{
-		String [][]paramsData = {{"Param1","Value1"},{"Name","Somik"},{"Age","23"}};
-		Hashtable paramsMap = new Hashtable();
-		String testHTML = new String("<APPLET CODE=Myclass.class ARCHIVE=test.jar CODEBASE=www.kizna.com>\n");
-		for (int i = 0;i<paramsData.length;i++)
-		{
-			testHTML+="<PARAM NAME=\""+paramsData[i][0]+"\" VALUE=\""+paramsData[i][1]+"\">\n";
-			paramsMap.put(paramsData[i][0],paramsData[i][1]);
-		}
-		testHTML+=
-			"</APPLET>\n"+
-			"</HTML>";
-		createParser(testHTML);
-		
-		// Register the applet scanner
-		parser.addScanner(new AppletScanner("-a"));
-			
-		parseAndAssertNodeCount(2);
-		assertTrue("Node should be an applet tag",node[0] instanceof AppletTag);
-		// Check the data in the applet tag
-		AppletTag appletTag = (AppletTag)node[0];
-		assertEquals("Class Name","Myclass.class",appletTag.getAppletClass());
-		assertEquals("Archive","test.jar",appletTag.getArchive());
-		assertEquals("Codebase","www.kizna.com",appletTag.getCodeBase());
-		// Check the params data
-		int cnt = 0;
-		for (Enumeration e = appletTag.getParameterNames();e.hasMoreElements();)
-		{
-			String paramName = (String)e.nextElement();
-			String paramValue = appletTag.getAttribute(paramName);
-			assertEquals("Param "+cnt+" value",paramsMap.get(paramName),paramValue);
-			cnt++;
-		}
-		assertEquals("Number of params",new Integer(paramsData.length),new Integer(cnt));
-	}
+    public void testScan() throws ParserException
+    {
+        String [][]paramsData = {{"Param1","Value1"},{"Name","Somik"},{"Age","23"}};
+        Hashtable paramsMap = new Hashtable();
+        String testHTML = new String("<APPLET CODE=Myclass.class ARCHIVE=test.jar CODEBASE=www.kizna.com>\n");
+        for (int i = 0;i<paramsData.length;i++)
+        {
+            testHTML+="<PARAM NAME=\""+paramsData[i][0]+"\" VALUE=\""+paramsData[i][1]+"\">\n";
+            paramsMap.put(paramsData[i][0],paramsData[i][1]);
+        }
+        testHTML+=
+            "</APPLET>\n"+
+            "</HTML>";
+        createParser(testHTML);
+        
+        // Register the applet scanner
+        parser.addScanner(new AppletScanner("-a"));
+            
+        parseAndAssertNodeCount(2);
+        assertTrue("Node should be an applet tag",node[0] instanceof AppletTag);
+        // Check the data in the applet tag
+        AppletTag appletTag = (AppletTag)node[0];
+        assertEquals("Class Name","Myclass.class",appletTag.getAppletClass());
+        assertEquals("Archive","test.jar",appletTag.getArchive());
+        assertEquals("Codebase","www.kizna.com",appletTag.getCodeBase());
+        // Check the params data
+        int cnt = 0;
+        for (Enumeration e = appletTag.getParameterNames();e.hasMoreElements();)
+        {
+            String paramName = (String)e.nextElement();
+            String paramValue = appletTag.getParameter(paramName);
+            assertEquals("Param "+cnt+" value",paramsMap.get(paramName),paramValue);
+            cnt++;
+        }
+        assertEquals("Number of params",new Integer(paramsData.length),new Integer(cnt));
+    }
 }
