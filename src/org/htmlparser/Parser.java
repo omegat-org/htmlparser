135d78624db5438e2efce8c26684e3087463e8dd
Derrick Oswald <derrickoswald@users.sourceforge.net>
2004-02-29 16:48:28 +0000

    Update version to 1.4-20040229

--- src/org/htmlparser/Parser.java
+++ src/org/htmlparser/Parser.java
@@ -87,7 +87,7 @@ public class Parser
      * The date of the version.
      */
     public final static String
-    VERSION_DATE = "Feb 16, 2004"
+    VERSION_DATE = "Feb 29, 2004"
     ;
 
     /**
